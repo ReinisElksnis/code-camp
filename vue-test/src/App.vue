37a71aeeaea67e141cc8ababd6c7e42edd051138
Reinis Elksnis <reinis.elksnis@gmail.com>
2022-10-06 19:29:30 +0300

    Button click demo.

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -2,6 +2,12 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div style="background: black;color:white">Hello</div>
+
+    <button @click="buttonClicked()">Click me</button>
+
+    <div>{{ counter }}</div>
   </div>
 </template>
 
@@ -10,8 +16,18 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      counter: 0
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    buttonClicked() {
+      this.counter = this.counter + 1;
+    }
   }
 }
 </script>
